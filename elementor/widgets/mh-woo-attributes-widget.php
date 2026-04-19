349c787abdc00e3bc3ad4796db882ed415b40dec
monabbor-hossen <monabborhossen@gmail.com>
2026-04-20 02:12:00 +0600

    update attributes

--- elementor/widgets/mh-woo-attributes-widget.php
+++ elementor/widgets/mh-woo-attributes-widget.php
@@ -4,189 +4,39 @@ if (!defined('ABSPATH')) {
 }
 
 /**
- * MH Plug - WooCommerce Product Attributes Widget
+ * MH Plug - WooCommerce Product Attributes Widget (With JS Bridge Fix)
  */
 class MH_Woo_Attributes_Widget extends \Elementor\Widget_Base
 {
 
-    public function get_name()
-    {
-        return 'mh_woo_attributes';
-    }
-
-    public function get_title()
-    {
-        return esc_html__('Product Attributes', 'mh-plug');
-    }
-
-    public function get_icon()
-    {
-        return 'eicon-product-meta';
-    }
-
-    public function get_categories()
-    {
-        return ['mh-plug-widgets'];
-    }
-
-    public function get_keywords()
-    {
-        return ['woocommerce', 'product', 'attributes', 'dropdown', 'select', 'mh'];
-    }
+    public function get_name() { return 'mh_woo_attributes'; }
+    public function get_title() { return esc_html__('Product Attributes', 'mh-plug'); }
+    public function get_icon() { return 'eicon-product-meta'; }
+    public function get_categories() { return ['mh-plug-widgets']; }
+    public function get_keywords() { return ['woocommerce', 'product', 'attributes', 'dropdown', 'select', 'mh']; }
 
     protected function register_controls()
     {
         /**
          * Label Styles
          */
-        $this->start_controls_section(
-            'section_style_label',
-            [
-                'label' => esc_html__('Label Style', 'mh-plug'),
-                'tab' => \Elementor\Controls_Manager::TAB_STYLE,
-            ]
-        );
-
-        $this->add_control(
-            'label_color',
-            [
-                'label' => esc_html__('Text Color', 'mh-plug'),
-                'type' => \Elementor\Controls_Manager::COLOR,
-                'default' => '#004265',
-                'selectors' => [
-                    '{{WRAPPER}} .mh-woo-attribute-label' => 'color: {{VALUE}};',
-                ],
-            ]
-        );
-
-        $this->add_group_control(
-            \Elementor\Group_Control_Typography::get_type(),
-            [
-                'name' => 'label_typography',
-                'selector' => '{{WRAPPER}} .mh-woo-attribute-label',
-            ]
-        );
-
-        $this->add_responsive_control(
-            'label_spacing',
-            [
-                'label' => esc_html__('Gap to Dropdown', 'mh-plug'),
-                'type' => \Elementor\Controls_Manager::SLIDER,
-                'size_units' => ['px', 'em'],
-                'range' => [
-                    'px' => ['min' => 0, 'max' => 50],
-                ],
-                'default' => ['unit' => 'px', 'size' => 10],
-                'selectors' => [
-                    '{{WRAPPER}} .mh-woo-attribute-label' => 'margin-bottom: {{SIZE}}{{UNIT}}; display: block;',
-                ],
-            ]
-        );
-
+        $this->start_controls_section( 'section_style_label', [ 'label' => esc_html__('Label Style', 'mh-plug'), 'tab' => \Elementor\Controls_Manager::TAB_STYLE, ] );
+        $this->add_control( 'label_color', [ 'label' => esc_html__('Text Color', 'mh-plug'), 'type' => \Elementor\Controls_Manager::COLOR, 'default' => '#004265', 'selectors' => [ '{{WRAPPER}} .mh-woo-attribute-label' => 'color: {{VALUE}};', ], ] );
+        $this->add_group_control( \Elementor\Group_Control_Typography::get_type(), [ 'name' => 'label_typography', 'selector' => '{{WRAPPER}} .mh-woo-attribute-label', ] );
+        $this->add_responsive_control( 'label_spacing', [ 'label' => esc_html__('Gap to Dropdown', 'mh-plug'), 'type' => \Elementor\Controls_Manager::SLIDER, 'size_units' => ['px', 'em'], 'range' => [ 'px' => ['min' => 0, 'max' => 50], ], 'default' => ['unit' => 'px', 'size' => 10], 'selectors' => [ '{{WRAPPER}} .mh-woo-attribute-label' => 'margin-bottom: {{SIZE}}{{UNIT}}; display: block;', ], ] );
         $this->end_controls_section();
 
         /**
          * Dropdown (<select>) Styles
          */
-        $this->start_controls_section(
-            'section_style_dropdown',
-            [
-                'label' => esc_html__('Dropdown Style', 'mh-plug'),
-                'tab' => \Elementor\Controls_Manager::TAB_STYLE,
-            ]
-        );
-
-        $this->add_control(
-            'dropdown_color',
-            [
-                'label' => esc_html__('Text Color', 'mh-plug'),
-                'type' => \Elementor\Controls_Manager::COLOR,
-                'default' => '#4a5568',
-                'selectors' => [
-                    '{{WRAPPER}} .mh-woo-attribute-select' => 'color: {{VALUE}};',
-                ],
-            ]
-        );
-
-        $this->add_control(
-            'dropdown_bg',
-            [
-                'label' => esc_html__('Background Color', 'mh-plug'),
-                'type' => \Elementor\Controls_Manager::COLOR,
-                'default' => '#e8edf2', // Neumorphic base
-                'selectors' => [
-                    '{{WRAPPER}} .mh-woo-attribute-select' => 'background-color: {{VALUE}};',
-                ],
-            ]
-        );
-
-        $this->add_group_control(
-            \Elementor\Group_Control_Typography::get_type(),
-            [
-                'name' => 'dropdown_typography',
-                'selector' => '{{WRAPPER}} .mh-woo-attribute-select',
-            ]
-        );
-
-        $this->add_responsive_control(
-            'dropdown_padding',
-            [
-                'label' => esc_html__('Padding', 'mh-plug'),
-                'type' => \Elementor\Controls_Manager::DIMENSIONS,
-                'size_units' => ['px', 'em', '%'],
-                'default' => [
-                    'top' => 12,
-                    'right' => 16,
-                    'bottom' => 12,
-                    'left' => 16,
-                    'isLinked' => false,
-                ],
-                'selectors' => [
-                    '{{WRAPPER}} .mh-woo-attribute-select' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
-                ],
-            ]
-        );
-
-        $this->add_control(
-            'dropdown_radius',
-            [
-                'label' => esc_html__('Border Radius', 'mh-plug'),
-                'type' => \Elementor\Controls_Manager::SLIDER,
-                'size_units' => ['px', '%'],
-                'default' => ['unit' => 'px', 'size' => 12],
-                'selectors' => [
-                    '{{WRAPPER}} .mh-woo-attribute-select' => 'border-radius: {{SIZE}}{{UNIT}};',
-                ],
-            ]
-        );
-
-        $this->add_control(
-            'dropdown_width',
-            [
-                'label' => esc_html__('Width', 'mh-plug'),
-                'type' => \Elementor\Controls_Manager::SLIDER,
-                'size_units' => ['px', '%'],
-                'default' => ['unit' => '%', 'size' => 100],
-                'selectors' => [
-                    '{{WRAPPER}} .mh-woo-attribute-select' => 'width: {{SIZE}}{{UNIT}};',
-                ],
-            ]
-        );
-
-        $this->add_control(
-            'dropdown_spacing',
-            [
-                'label' => esc_html__('Spacing Between Attributes', 'mh-plug'),
-                'type' => \Elementor\Controls_Manager::SLIDER,
-                'size_units' => ['px', 'em'],
-                'default' => ['unit' => 'px', 'size' => 20],
-                'selectors' => [
-                    '{{WRAPPER}} .mh-woo-attribute-wrapper' => 'margin-bottom: {{SIZE}}{{UNIT}};',
-                    '{{WRAPPER}} .mh-woo-attribute-wrapper:last-child' => 'margin-bottom: 0;',
-                ],
-            ]
-        );
-
+        $this->start_controls_section( 'section_style_dropdown', [ 'label' => esc_html__('Dropdown Style', 'mh-plug'), 'tab' => \Elementor\Controls_Manager::TAB_STYLE, ] );
+        $this->add_control( 'dropdown_color', [ 'label' => esc_html__('Text Color', 'mh-plug'), 'type' => \Elementor\Controls_Manager::COLOR, 'default' => '#4a5568', 'selectors' => [ '{{WRAPPER}} .mh-woo-attribute-select' => 'color: {{VALUE}};', ], ] );
+        $this->add_control( 'dropdown_bg', [ 'label' => esc_html__('Background Color', 'mh-plug'), 'type' => \Elementor\Controls_Manager::COLOR, 'default' => '#e8edf2', 'selectors' => [ '{{WRAPPER}} .mh-woo-attribute-select' => 'background-color: {{VALUE}};', ], ] );
+        $this->add_group_control( \Elementor\Group_Control_Typography::get_type(), [ 'name' => 'dropdown_typography', 'selector' => '{{WRAPPER}} .mh-woo-attribute-select', ] );
+        $this->add_responsive_control( 'dropdown_padding', [ 'label' => esc_html__('Padding', 'mh-plug'), 'type' => \Elementor\Controls_Manager::DIMENSIONS, 'size_units' => ['px', 'em', '%'], 'default' => [ 'top' => 12, 'right' => 16, 'bottom' => 12, 'left' => 16, 'isLinked' => false, ], 'selectors' => [ '{{WRAPPER}} .mh-woo-attribute-select' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};', ], ] );
+        $this->add_control( 'dropdown_radius', [ 'label' => esc_html__('Border Radius', 'mh-plug'), 'type' => \Elementor\Controls_Manager::SLIDER, 'size_units' => ['px', '%'], 'default' => ['unit' => 'px', 'size' => 12], 'selectors' => [ '{{WRAPPER}} .mh-woo-attribute-select' => 'border-radius: {{SIZE}}{{UNIT}};', ], ] );
+        $this->add_control( 'dropdown_width', [ 'label' => esc_html__('Width', 'mh-plug'), 'type' => \Elementor\Controls_Manager::SLIDER, 'size_units' => ['px', '%'], 'default' => ['unit' => '%', 'size' => 100], 'selectors' => [ '{{WRAPPER}} .mh-woo-attribute-select' => 'width: {{SIZE}}{{UNIT}};', ], ] );
+        $this->add_control( 'dropdown_spacing', [ 'label' => esc_html__('Spacing Between Attributes', 'mh-plug'), 'type' => \Elementor\Controls_Manager::SLIDER, 'size_units' => ['px', 'em'], 'default' => ['unit' => 'px', 'size' => 20], 'selectors' => [ '{{WRAPPER}} .mh-woo-attribute-wrapper' => 'margin-bottom: {{SIZE}}{{UNIT}};', '{{WRAPPER}} .mh-woo-attribute-wrapper:last-child' => 'margin-bottom: 0;', ], ] );
         $this->end_controls_section();
     }
 
@@ -217,7 +67,6 @@ class MH_Woo_Attributes_Widget extends \Elementor\Widget_Base
 
         foreach ($attributes as $attribute) {
             
-            // 🚀 THE FIX: Calculate exactly what WooCommerce expects for the name attribute
             $attribute_name = $attribute->get_name();
             $label          = wc_attribute_label($attribute_name);
             $select_id      = sanitize_title($attribute_name);
@@ -226,7 +75,6 @@ class MH_Woo_Attributes_Widget extends \Elementor\Widget_Base
             echo '<div class="mh-woo-attribute-wrapper">';
             echo '<label class="mh-woo-attribute-label" for="' . esc_attr($select_id) . '" style="font-weight:600;">' . esc_html($label) . '</label>';
             
-            // 🚀 THE FIX: Add the ID, NAME, and DATA-ATTRIBUTE_NAME to the select tag
             echo '<select id="' . esc_attr($select_id) . '" name="' . esc_attr($select_name) . '" class="mh-woo-attribute-select" style="' . esc_attr($default_select_css) . '" data-attribute_name="' . esc_attr($select_name) . '">';
             
             echo '<option value="">' . esc_html(sprintf(__('Choose %s', 'mh-plug'), $label)) . '</option>';
@@ -255,5 +103,49 @@ class MH_Woo_Attributes_Widget extends \Elementor\Widget_Base
         }
 
         echo '</div>';
+
+        // 🚀 THE FIX: JavaScript Bridge to connect these custom dropdowns directly to the WooCommerce Form
+        ?>
+        <script>
+            jQuery(document).ready(function($) {
+                
+                function syncWooAttributes() {
+                    $('.mh-woo-attribute-select').each(function() {
+                        var name = $(this).attr('name');
+                        var val = $(this).val();
+                        var $form = $('form.cart'); // Find the official WooCommerce Add to Cart form
+
+                        if ($form.length) {
+                            // Check if this is a Variable Product (WooCommerce generates hidden selects for these)
+                            var $nativeSelect = $form.find('select[name="' + name + '"]');
+                            
+                            if ($nativeSelect.length) {
+                                // Variable product: Update WooCommerce's native dropdown and trigger the change event to calculate price
+                                $nativeSelect.val(val).trigger('change');
+                            } else {
+                                // Simple product with custom attributes: Inject hidden inputs directly into the form!
+                                var $hiddenInput = $form.find('input[name="' + name + '"]');
+                                
+                                if ($hiddenInput.length) {
+                                    $hiddenInput.val(val); // Update existing hidden input
+                                } else if (val !== '') {
+                                    $form.append('<input type="hidden" name="' + name + '" value="' + val + '">'); // Create new hidden input
+                                }
+                            }
+                        }
+                    });
+                }
+
+                // Run it once immediately in case there are default options selected
+                syncWooAttributes();
+
+                // Run it every time the customer changes a dropdown
+                $('.mh-woo-attribute-select').on('change', function() {
+                    syncWooAttributes();
+                });
+                
+            });
+        </script>
+        <?php
     }
 }
