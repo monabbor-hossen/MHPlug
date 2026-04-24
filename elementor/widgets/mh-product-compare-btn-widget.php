b988da5bf5c2431d814e7875be1283688cfa224e
monabbor-hossen <monabborhossen@gmail.com>
2026-04-24 22:13:52 +0600

    update compare

--- elementor/widgets/mh-product-compare-btn-widget.php
+++ elementor/widgets/mh-product-compare-btn-widget.php
@@ -16,6 +16,9 @@ class MH_Product_Compare_Btn_Widget extends Widget_Base {
     public function get_script_depends() { return [ 'mh-widgets-js' ]; }
 
     protected function register_controls() {
+        // ==========================================
+        // CONTENT TAB
+        // ==========================================
         $this->start_controls_section('section_content', ['label' => __('Content', 'mh-plug')]);
         
         $this->add_control('text_normal', [
@@ -43,16 +46,44 @@ class MH_Product_Compare_Btn_Widget extends Widget_Base {
         
         $this->end_controls_section();
 
+        // ==========================================
+        // STYLE TAB
+        // ==========================================
         $this->start_controls_section('section_style', ['label' => __('Button Style', 'mh-plug'), 'tab' => Controls_Manager::TAB_STYLE]);
-        $this->add_group_control(Group_Control_Typography::get_type(), ['name' => 'typography', 'selector' => '{{WRAPPER}} .mh-compare-btn']);
         
+        // --- TYPOGRAPHY (TEXT SIZE & FONT) ---
+        $this->add_control('heading_text_style', ['label' => __('Text Settings', 'mh-plug'), 'type' => Controls_Manager::HEADING]);
+        $this->add_group_control(Group_Control_Typography::get_type(), [
+            'name' => 'typography', 
+            'label' => __('Text Typography & Size', 'mh-plug'),
+            'selector' => '{{WRAPPER}} .mh-compare-btn .mh-compare-text'
+        ]);
+
+        // --- ICON SETTINGS ---
+        $this->add_control('heading_icon_style', ['label' => __('Icon Settings', 'mh-plug'), 'type' => Controls_Manager::HEADING, 'separator' => 'before']);
+        $this->add_responsive_control('icon_size', [
+            'label' => __('Icon Size', 'mh-plug'),
+            'type' => Controls_Manager::SLIDER,
+            'range' => ['px' => ['min' => 10, 'max' => 100]],
+            'selectors' => [
+                '{{WRAPPER}} .mh-compare-btn i' => 'font-size: {{SIZE}}{{UNIT}};',
+                '{{WRAPPER}} .mh-compare-btn svg' => 'width: {{SIZE}}{{UNIT}}; height: {{SIZE}}{{UNIT}};',
+            ],
+        ]);
+        $this->add_responsive_control('icon_spacing', [
+            'label' => __('Icon Spacing (Gap)', 'mh-plug'),
+            'type' => Controls_Manager::SLIDER,
+            'selectors' => ['{{WRAPPER}} .mh-compare-btn' => 'gap: {{SIZE}}{{UNIT}};'],
+        ]);
+
+        // --- BUTTON SPACING ---
+        $this->add_control('heading_box_style', ['label' => __('Box Settings', 'mh-plug'), 'type' => Controls_Manager::HEADING, 'separator' => 'before']);
         $this->add_responsive_control('padding', [
             'label' => __('Padding', 'mh-plug'),
             'type' => Controls_Manager::DIMENSIONS,
             'size_units' => ['px', 'em', '%'],
             'selectors' => ['{{WRAPPER}} .mh-compare-btn' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};'],
         ]);
-
         $this->add_responsive_control('border_radius', [
             'label' => __('Border Radius', 'mh-plug'),
             'type' => Controls_Manager::DIMENSIONS,
@@ -60,20 +91,22 @@ class MH_Product_Compare_Btn_Widget extends Widget_Base {
             'selectors' => ['{{WRAPPER}} .mh-compare-btn' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};'],
         ]);
 
-        $this->add_responsive_control('icon_spacing', [
-            'label' => __('Icon Spacing', 'mh-plug'),
-            'type' => Controls_Manager::SLIDER,
-            'selectors' => ['{{WRAPPER}} .mh-compare-btn' => 'gap: {{SIZE}}{{UNIT}};'],
-        ]);
-
+        // ==========================================
+        // NORMAL / HOVER / ADDED TABS
+        // ==========================================
         $this->start_controls_tabs('tabs_btn_style');
 
-        // NORMAL
+        // --- NORMAL ---
         $this->start_controls_tab('tab_btn_normal', ['label' => __('Normal', 'mh-plug')]);
         $this->add_control('text_color', [
-            'label' => __('Text/Icon Color', 'mh-plug'),
+            'label' => __('Text Color', 'mh-plug'),
             'type' => Controls_Manager::COLOR,
-            'selectors' => ['{{WRAPPER}} .mh-compare-btn' => 'color: {{VALUE}}; fill: {{VALUE}};'],
+            'selectors' => ['{{WRAPPER}} .mh-compare-btn .mh-compare-text' => 'color: {{VALUE}};'],
+        ]);
+        $this->add_control('icon_color', [
+            'label' => __('Icon Color', 'mh-plug'),
+            'type' => Controls_Manager::COLOR,
+            'selectors' => ['{{WRAPPER}} .mh-compare-btn i' => 'color: {{VALUE}};', '{{WRAPPER}} .mh-compare-btn svg' => 'fill: {{VALUE}};'],
         ]);
         $this->add_control('bg_color', [
             'label' => __('Background Color', 'mh-plug'),
@@ -84,12 +117,17 @@ class MH_Product_Compare_Btn_Widget extends Widget_Base {
         $this->add_group_control(Group_Control_Box_Shadow::get_type(), ['name' => 'shadow_normal', 'selector' => '{{WRAPPER}} .mh-compare-btn']);
         $this->end_controls_tab();
 
-        // HOVER
+        // --- HOVER ---
         $this->start_controls_tab('tab_btn_hover', ['label' => __('Hover', 'mh-plug')]);
         $this->add_control('text_color_hover', [
-            'label' => __('Text/Icon Color', 'mh-plug'),
+            'label' => __('Text Color', 'mh-plug'),
             'type' => Controls_Manager::COLOR,
-            'selectors' => ['{{WRAPPER}} .mh-compare-btn:hover' => 'color: {{VALUE}}; fill: {{VALUE}};'],
+            'selectors' => ['{{WRAPPER}} .mh-compare-btn:hover .mh-compare-text' => 'color: {{VALUE}};'],
+        ]);
+        $this->add_control('icon_color_hover', [
+            'label' => __('Icon Color', 'mh-plug'),
+            'type' => Controls_Manager::COLOR,
+            'selectors' => ['{{WRAPPER}} .mh-compare-btn:hover i' => 'color: {{VALUE}};', '{{WRAPPER}} .mh-compare-btn:hover svg' => 'fill: {{VALUE}};'],
         ]);
         $this->add_control('bg_color_hover', [
             'label' => __('Background Color', 'mh-plug'),
@@ -104,12 +142,17 @@ class MH_Product_Compare_Btn_Widget extends Widget_Base {
         ]);
         $this->end_controls_tab();
 
-        // ADDED (ACTIVE)
+        // --- ADDED (ACTIVE) ---
         $this->start_controls_tab('tab_btn_added', ['label' => __('Added', 'mh-plug')]);
         $this->add_control('text_color_added', [
-            'label' => __('Text/Icon Color', 'mh-plug'),
+            'label' => __('Text Color', 'mh-plug'),
             'type' => Controls_Manager::COLOR,
-            'selectors' => ['{{WRAPPER}} .mh-compare-btn.added' => 'color: {{VALUE}}; fill: {{VALUE}};'],
+            'selectors' => ['{{WRAPPER}} .mh-compare-btn.added .mh-compare-text' => 'color: {{VALUE}};'],
+        ]);
+        $this->add_control('icon_color_added', [
+            'label' => __('Icon Color', 'mh-plug'),
+            'type' => Controls_Manager::COLOR,
+            'selectors' => ['{{WRAPPER}} .mh-compare-btn.added i' => 'color: {{VALUE}};', '{{WRAPPER}} .mh-compare-btn.added svg' => 'fill: {{VALUE}};'],
         ]);
         $this->add_control('bg_color_added', [
             'label' => __('Background Color', 'mh-plug'),
@@ -127,7 +170,7 @@ class MH_Product_Compare_Btn_Widget extends Widget_Base {
             'type' => Controls_Manager::SLIDER,
             'default' => ['size' => 0.3],
             'range' => ['px' => ['max' => 3, 'step' => 0.1]],
-            'selectors' => ['{{WRAPPER}} .mh-compare-btn' => 'transition: all {{SIZE}}s ease;'],
+            'selectors' => ['{{WRAPPER}} .mh-compare-btn, {{WRAPPER}} .mh-compare-btn i, {{WRAPPER}} .mh-compare-btn svg, {{WRAPPER}} .mh-compare-btn .mh-compare-text' => 'transition: all {{SIZE}}s ease;'],
             'separator' => 'before',
         ]);
         
